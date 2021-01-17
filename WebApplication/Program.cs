728acb8eb5eeb6c34c3dfdabedfc8774e59392fa
pavel-vinogradov <vini007@mail.ru>
2021-01-17 16:55:36 +0300

    setting project

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using VueCliMiddleware;
 
 namespace WebApplication
 {
@@ -13,11 +9,22 @@ namespace WebApplication
     {
         public static void Main(string[] args)
         {
+            if (!CommandLine.Arguments.TryGetOptions(args, true, out string mode, out ushort port, out bool https)) return;
+
+            if (mode == "kill") {
+                Console.WriteLine($"Killing process serving port {port}...");
+                PidUtils.KillPort(port, true, true);
+                return;
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
