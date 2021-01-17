909ce07695eb697f81b47f13de2513d37dcc3cb9
pavel-vinogradov <vini007@mail.ru>
2021-01-17 21:16:05 +0300

    finish customs asp.net core + vue.js

---
+++ ClientApp/src/main.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import router from './router'
+import store from '@/store/index'
+import './registerServiceWorker'
+import dateFilter from '@/filters/date.filter'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/style/main.scss'
+Vue.config.productionTip = false
+
+Vue.filter('date', dateFilter)
+
+new Vue({
+   router,
+  store,
+  render: (h) => h(App)
+}).$mount('#app')
